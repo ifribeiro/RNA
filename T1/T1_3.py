027adce613eb7034ed9f2cbed37bf3d11c686921
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2022-10-26 20:57:21 -0300

    T1_3

--- T1/T1_3.py
+++ T1/T1_3.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sympy import *
 import matplotlib.animation as animation
+plt.style.use("seaborn")
 
 def gradient_descent(x0, f, alpha, gmin, kmax):
     """
@@ -28,7 +29,6 @@ f1 = lambdify(x,f)
 X = np.linspace(1, 3, 20)
 Y = [exp(-xi)*xi*(xi**2-xi-1) for xi in X]
 
-
 # calculando gradiente
 Xgradiente = gradient_descent(x0=3, f=f1, alpha=0.1, gmin=0.1, kmax=20)
 Ygradiente = [exp(-xi)*xi*(xi**2-xi-1) for xi in Xgradiente]
@@ -44,11 +44,8 @@ def animate(i):
     y = Ygradiente[i]
     ax.scatter([x], [y], c='green')
     flinha = f1(x)
-    print ('flinha', flinha)
-    print ('x', x)
-    print ('y', y)
     ax.set_title("$x{}:{:.3f}, f(x{}):{:.3f}, f'(x{}):{:.3f}$".format(i, x, i, y, i, flinha))
-   
-ani = animation.FuncAnimation(fig, animate, frames=np.arange(1,len(Xgradiente)), interval=1000)
-ani.save("gd.gif")
+print ("Valor de minimo obtido: {:.3f}".format(Ygradiente[-1]))
+ani = animation.FuncAnimation(fig, animate, frames=np.arange(1,len(Xgradiente)), interval=1000, repeat=False)
+# ani.save("gd.gif")
 plt.show() 
