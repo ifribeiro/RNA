5199b227cb6d17fdb1a9339acdbb14f9cc428f22
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2022-10-27 10:57:22 -0300

    finalizando item 4

--- T1/T1_4.py
+++ T1/T1_4.py
@@ -1,145 +1,127 @@
-from re import X
+from numpy.linalg import norm
 import numpy as np
 from sympy import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 
-# np.random.seed(18)
+np.random.seed(0)
 
 def f(x1, x2):
-    return (4 - 2.1 * x1**2 + x1**2 / 3 ) * x1**3 + x1 * x2 + (-4 + 4 * x2**2) * x2**2
+    return (4.0 - 2.1 * x1**2 + x1**2/ 3.0 ) * x1**3 + x1 * x2 + (-4.0 + 4.0 * x2**2) * x2**2
+
 def dif_finitas(func, x1_k, x2_k, h):
     """
     Método de diferenças finitas
     """
-    d = func(x1_k+h, x2_k+h) - f(x1_k, x2_k)
-    return d
+    gradiente = np.array([0.0,0.0])
+    gradiente[0] = (func(x1_k+h, x2_k) - func(x1_k, x2_k))/h
+    gradiente[1] = (func(x1_k, x2_k+h) - func(x1_k, x2_k))/h
+    return gradiente
+
+def grad_f(f1, f2, x1_k, x2_k):
+    gradiente = np.array([f1(x1_k, x2_k),f2(x1_k, x2_k)])
+    return gradiente
 
 def gradient_descent(x1_0, x2_0, f, alpha, gmin, kmax, diff_fin=True):
     """
-    x1_0: valor inicial x1
-    x2_0: valor inicial x2
-    f: derivada da função f (lambdify)
+    Descida do gradiente
+
+    Params:
+
+        - x1_0: valor inicial x1
+        - x2_0: valor inicial x2
+        - f: função (para o método de diferenças finitas) ou lista de derivadas
+        - alpha: velocidade da descida
+        - gmin: limiar
+        - diff_fin: usar diferenças finitas ou não
     """
-    k = 0
-    xk_1, xk_2 = [x1_0, x2_0]
-    X = [[xk_1, xk_2]]
-    while (k < kmax):        
+    k = 0    
+    X = np.zeros((kmax+1, 2))
+    # print (x1_0, x2_0)
+    X[0][0], X[0][1] = [x1_0, x2_0]
+    if (diff_fin):
+        g = dif_finitas(f, X[k][0],X[k][1], 0.01)
+    else:
+        # para derivadas, f será um vetor com 2 funções
+        g = grad_f(f[0], f[1], X[k][0],X[k][1])
+    while (k < kmax) and (norm(g)>gmin):        
         # se o gradiente for obtido por diferenças finitas
-        # TODO: Rever lógica de parada para diferencas finitas
         if (diff_fin):
-            g = dif_finitas(f,xk_1, xk_2, 0.01)
-            print (abs(g), gmin)
-            # print (abs(g)<=gmin)
-            if abs(g) <= gmin: 
-                print ("Parando antes")
-                break
-            # else: 
-            #     # usar função normal
-            #     g = dif_finitas(f,xk[0], xk[1], 0.01)
-        else:   
-            # usar função simbolica (definida com sympy)          
-            g = f(xk_1,xk_2)
-        xk_1 -= alpha*abs(g) 
-        xk_2 -= alpha*abs(g) 
-        X.append([xk_1, xk_2])
+            g = dif_finitas(f, X[k][0],X[k][1], 0.01)
+        else:           
+            g = grad_f(f[0], f[1], X[k][0],X[k][1])
+        X[k+1] = X[k] - alpha*g
         k+=1
-        # print (g)
-    return X
+    return X[:k]
+
+def get_XYZ_3d(f, limx, limy, N):
+    X1 = np.linspace(limx,limy,N)
+    X2 = np.linspace(limx,limy,N)
+    X1, X2 = np.meshgrid(X1, X2)
+    zarray = []
+    for i in range(len(X1)):
+        zarray_interno = []
+        for j in range(len(X1)):
+            z = f(X1[i][j],X2[i][j])
+            zarray_interno.append(z)
+        zarray.append(zarray_interno)
+    Z = np.array(zarray)
+    return X1, X2, Z
 
 
 def item_a(diff_fin=True, f=None):
-    X0 = np.random.uniform(low=0, high=5.5, size=2)
-    # X0 = [-0.99530908  1.30724695]
-    # X0 = [0, -2]  
-    # X0 = [1.90047238, 3.87944662]
-    # X0 = [-0.64231882 , 0.57173843]
-    X_gradient = gradient_descent(X0[0], X0[1], f, 0.2, gmin=0.1, kmax=70)
-    # if diff_fin
-    # TODO: fazer dois tipos
-    Y_gradient = [f(x[0], x[1]) for x in X_gradient]
-    return X_gradient, Y_gradient
-
-def item_b():
-    # criando um símbolo
-    x1 = Symbol('x1')
-    x2 = Symbol('x2')
-    # definindo a funcao
-    f = (4 - 2.1 * x1**2 + x1**2 / 3 ) * x1**3 + x1 * x2 + (-4 + 4 * x2**2) * x2**2
-    f1 = lambdify([x1, x2],f)
-    X0 = np.random.uniform(low=-1, high=1.1, size=2)
-    print (X0)
-    # X0 = [-1.0, -.5]
-    Xgradiente = gradient_descent(X0[0], X0[1], f=f1, alpha=0.1, gmin=0.1, kmax=20, diff_fin=False)
-
-X1 = np.linspace(-.75,.75,1000)
-X2 = np.linspace(-.75,.75,1000)
-
-X1, X2 = np.meshgrid(X1, X2)
-
-# Z = [f(Xi[0], Xi[1]) for Xi in zip(X1, X2)]
-zarray = []
-for i in range(len(X1)):
-    zarray_interno = []
-    for j in range(len(X1)):
-        z = f(X1[i][j],X2[i][j])
-        zarray_interno.append(z)
-    zarray.append(zarray_interno)
-Z = np.array(zarray)
-
-
-def item_c():
     """
-    Plota X1, X2 e o valor da função aplicando o gradiente
+    Realiza a descida do gradiente utilizando difereças finitas
     """
-    # plot the surface
+    low=-3
+    high=3
+    X0 = np.random.uniform(low=low, high=high, size=2)    
+    X1, X2, Z = get_XYZ_3d(f, -1.5, 1.2, 500)
+    X_gradient = gradient_descent(X0[0], X0[1], f, 0.1, gmin=0.1, kmax=20)
+    Y_gradient = [f(x[0], x[1]) for x in X_gradient]    
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    # plt3d = plt.figure().gca(projection='3d')
-    # ax.plot_surface(X1, X2, Z, alpha=0.2)
+    ax.plot_surface(X1, X2, Z, alpha=0.4, cmap='jet', edgecolor=None)
+    ax.plot(X_gradient[:, 0],X_gradient[:, 1], Y_gradient, label='Desc. Grad.')
+    ax.scatter(X_gradient[0, 0],X_gradient[0, 1], Y_gradient[0], c='green', label='Início')
+    ax.scatter(X_gradient[-1, 0],X_gradient[-1, 1], Y_gradient[-1], c='red', label='Fim')
     ax.set_xlabel("X1")
     ax.set_ylabel("X2")
-    # ax.set_ylabel("X2")
-
-    Xg, Yg = item_a()
-    Xg = np.array(Xg)
-    ax.plot(Xg[:, 0], Xg[:, 1], Yg)
-    ax.plot(Xg[:, 0], Xg[:, 1], np.zeros_like(Yg), c='red')
-    ax.scatter([Xg[0, 0]], [Xg[0, 1]], c='green')
-    ax.scatter([Xg[-1, 0]], [Xg[-1, 1]], c='black')
-    
-    # def animate(i):
-    #     ax.clear()
-    #     ax.plot_surface(X1, X2, Z, alpha=0.2)
-    #     x1, x2 = Xg[i]
-    #     y = Yg[i]
-    #     ax.scatter([x1], [x2], [y], c='green')
-    #     # flinha = f1(x)
-    #     # print ('flinha', flinha)
-        
-    #     ax.set_title("k:{}".format(i)) 
-    
-    # ani = animation.FuncAnimation(fig, animate, frames=np.arange(1,len(Xg)), interval=500)
-
-    # Ensure that the next plot doesn't overwrite the first plot  
-
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.plot
-    # ax.plot(X1, X2, Z)
+    plt.legend()
+    plt.title("Descida do Gradiente por diferenças finitas")
+    plt.savefig("t1_4_item_A.jpeg")
     plt.show()
-    
-# item a
-# X0 = [-0.78764362 , -0.65387373]
-# X0 = [-0.64231882 , 0.57173843]
-# X0 = [1.90047238 3.87944662] Descida bonita
+    return X_gradient, Y_gradient
 
-# xg, yg = item_a()
-# Fx = [f(X[0], X[1]) for X in xg]
-# plt.plot(Fx)
-# plt.show()
+def item_b(func):
+    # criando os símbolos
+    x1, x2 = symbols('x1 x2')
+    # definindo a funcao
+    f = (4.0 - 2.1 * x1**2 + x1**2 / 3.0 ) * x1**3 + x1 * x2 + (-4.0 + 4.0 * x2**2) * x2**2
+    # derivadas parciais
+    d1 = f.diff(x1)
+    d2 = f.diff(x2) 
+    fd1 = lambdify([x1, x2], d1) 
+    fd2 = lambdify([x1, x2], d2) 
+    X0 = np.random.uniform(low=-3, high=3, size=2)
+    # X0 = [-1.0, -.5]
+    X_gradient = gradient_descent(X0[0], X0[1], f=[fd1, fd2], alpha=0.1, gmin=0.1, kmax=20, diff_fin=False)
+    Y_gradient = [func(x[0], x[1]) for x in X_gradient]
 
-# print (xg)
-# item_b()
-item_c()
+    X1, X2, Z = get_XYZ_3d(func, -1.5, 1.2, 500)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X1, X2, Z, alpha=0.4, cmap='jet', edgecolor=None)
+    ax.plot(X_gradient[:, 0],X_gradient[:, 1], Y_gradient, label='Desc. Grad.')
+    ax.scatter(X_gradient[0, 0],X_gradient[0, 1], Y_gradient[0], c='green', label='Início')
+    ax.scatter(X_gradient[-1, 0],X_gradient[-1, 1], Y_gradient[-1], c='red', label='Fim')
+    ax.set_xlabel("X1")
+    ax.set_ylabel("X2")
+    plt.legend()
+    plt.title("Descida do Gradiente por derivadas")
+    plt.savefig("t1_4_item_B.jpeg")
+    plt.show()
+xg, yg = item_a(f=f)
+item_b(f)
